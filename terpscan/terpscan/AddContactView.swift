c0709a617fd8bad951ad8a79d82e3b622ee9f794
Justin Le <justinqle@gmail.com>
2020-03-18 02:44:20 -0400

    AddContactView template

---
+++ terpscan/terpscan/AddContactView.swift
@@ -0,0 +1,21 @@
+//
+//  AddContactView.swift
+//  terpscan
+//
+//  Created by Justin Le on 3/18/20.
+//  Copyright © 2020 Justin Le. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddContactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddContactView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddContactView()
+    }
+}
