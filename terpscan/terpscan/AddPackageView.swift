f0941ddf1894048737ab5d10f8a93c02a1ac2364
Justin Le <justinqle@gmail.com>
2020-03-19 01:39:25 -0400

    Barcode scanning bugs

--- terpscan/terpscan/AddPackageView.swift
+++ terpscan/terpscan/AddPackageView.swift
@@ -16,6 +16,9 @@ extension String {
         let endIndex = self.index(self.startIndex, offsetBy: to)
         return String(self[startIndex...endIndex])
     }
+    var isAlphanumeric: Bool {
+        return !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
+    }
 }
 
 struct AddPackageView: View {
@@ -101,14 +104,15 @@ struct AddPackageView: View {
     func handleScan(result: Result<String, CodeScannerView.ScanError>) {
         self.isShowingScanner = false
         switch result {
-        case .success(let code):
+        case .success(var code):
             print(code)
             // Further parse barcode if needed
-            if (code.hasPrefix("[)>")) { // FedEx PDF417 barcode
-                self.trackingNumber = (code.filter { !$0.isNewline && !$0.isWhitespace }).subString(from: 22, to: 34)
+            code = code.filter { String($0).isAlphanumeric }
+            if (code.hasPrefix("0102")) { // FedEx PDF417 barcode
+                self.trackingNumber = code.subString(from: 14, to: 26)
             } else if code.hasPrefix("96")  { // Also FedEx
                 self.trackingNumber = code.subString(from: code.count-12, to: code.count-1)
-            } else if (code.hasPrefix("420")) {
+            } else if (code.hasPrefix("420") && code.count > 8) { // USPS
                 self.trackingNumber = code.subString(from: 8, to: code.count-1)
             } else {
                 self.trackingNumber = code
@@ -122,6 +126,8 @@ struct AddPackageView: View {
                 selectedCarrier = 2
             } else if self.trackingNumber.hasPrefix("FBA") { // Amazon
                 selectedCarrier = 3
+            } else {
+                selectedCarrier = 5
             }
         case .failure(let error):
             print("Scanning failed with \(error)")
